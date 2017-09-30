5f0024d73b75a438515e114e01e82dfa8cf01d8d
Emmanuel King Kasulani <kasulani@gmail.com>
2017-09-30 14:39:10 +0300

    add signup page

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 import { Route } from "react-router-dom";
 import HomePage from "./components/pages/HomePage";
+import SignupPage from "./components/pages/SignupPage";
 
 const App = () => (<div>
     <Route path="/" exact component={HomePage} />
+    <Route path="/signup" exact component={SignupPage} />
 </div>);
 
 export default App;
