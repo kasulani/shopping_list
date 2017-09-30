fca0ddbbd5721b5ede326706e6b0ce5014f52bc6
Emmanuel King Kasulani <kasulani@gmail.com>
2017-09-30 14:30:40 +0300

    add home page

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,9 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import HomePage from "./components/pages/HomePage";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      </div>
-    );
-  }
-}
+const App = () => (<div>
+    <Route path="/" exact component={HomePage} />
+</div>);
 
 export default App;
---
+++ app/src/components/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Link} from "react-router-dom"
+
+const HomePage = () => (
+    <div>
+        <h1>Shopping List</h1>
+        <Link to="/signup">signup</Link>
+    </div>
+);
+
+export default HomePage;
