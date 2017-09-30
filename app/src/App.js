4c3b6e7d1684b9a146aa43abb1657ac308d0ea7b
Emmanuel King Kasulani <kasulani@gmail.com>
2017-09-30 14:52:16 +0300

    add semantic-ui

--- app/src/App.js
+++ app/src/App.js
@@ -3,9 +3,11 @@ import { Route } from "react-router-dom";
 import HomePage from "./components/pages/HomePage";
 import SignupPage from "./components/pages/SignupPage";
 
-const App = () => (<div>
-    <Route path="/" exact component={HomePage} />
-    <Route path="/signup" exact component={SignupPage} />
-</div>);
+const App = () => (
+    <div className="ui container">
+        <Route path="/" exact component={HomePage} />
+        <Route path="/signup" exact component={SignupPage} />
+    </div>
+);
 
 export default App;
