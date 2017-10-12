0a1a42fcfe8e20269235d93de20bba8540d6dd58
Emmanuel King Kasulani <kasulani@gmail.com>
2017-10-12 16:55:00 +0300

    add signup page route

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import HomePage from './components/home/HomePage';
+import SignupPage from './components/signup/SignupPage';
 
 export default (
   <Route path="/" component={App}>
     {/*pass these as children to the App component based on the path*/}
     <IndexRoute component={HomePage}/>
+    <Route path="signup" component={SignupPage}/>
   </Route>
 );
