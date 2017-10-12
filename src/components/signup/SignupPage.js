80e0ced87333079d3a72c3df02c332f43c6fcf2f
Emmanuel King Kasulani <kasulani@gmail.com>
2017-10-12 15:02:13 +0300

    add signup page

---
+++ src/components/signup/SignupPage.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Link } from 'react-dom';
+import TextInput from '../common/TextInput';
+
+class HomePage extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return(
+      <div className="jumbotron">
+        <h1>Shopping List</h1>
+        <div className="row">
+          <div className="col-lg-8">
+            <p className="lead">
+              This is an application that allows you to record and share things you want to
+              spend your money on and keep track of your shopping lists and the best part of
+              it all is that you can share your Shopping List with your friends.
+            </p>
+            <p><a className="btn btn-lg btn-success" href="#" role="button">Sign up today</a></p>
+          </div>
+          <div className="col-lg-4">
+            <h4>Signup</h4>
+            <form>
+              <TextInput
+                name="username"
+                label="username"
+                />
+              <TextInput
+                name="password1"
+                label="password"
+              />
+              <TextInput
+                name="password2"
+                label="confirm password"
+              />
+              <input type="submit" value="Login" className="btn btn-sm btn-primary" role="button"/>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
