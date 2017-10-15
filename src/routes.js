ad46006bd034ae81d9104ae04a6055fd741f3258
Emmanuel King Kasulani <kasulani@gmail.com>
2017-10-15 16:36:50 +0300

    add route to home page

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ export default (
   <Route path="/" component={App}>
     {/*pass these as children to the App component based on the path*/}
     <IndexRoute component={HomePage}/>
+    <Route path="/" component={HomePage}/>
     <Route path="signup" component={SignupPage}/>
   </Route>
 );
