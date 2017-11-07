2a8b65324e8ac769140d993c1aa596df13f49984
kasulani <kasulani@gmail.com>
2017-11-07 19:58:53 +0300

    adds redux store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import 'babel-polyfill'; // to transpile features babel can't transpile from E6 to E5
 import React from 'react';
-import { render } from 'react-dom';
-import { Router, browserHistory } from 'react-router';
+import {render} from 'react-dom';
+import {Router, browserHistory} from 'react-router';
 import routes from './routes';
 import './styles/shoplist.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import configStore from "./store/configStore";
+import {Provider} from "react-redux";
+
+const store = configStore();
 
 render(
-  <Router history={browserHistory} routes={routes}/>, document.getElementById('app')
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes}/>
+  </Provider>, document.getElementById('app')
 );
