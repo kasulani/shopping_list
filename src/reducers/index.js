991b7260b86c9cb1b363ad7b170e82f7827fe4c9
kasulani <kasulani@gmail.com>
2017-11-07 20:06:32 +0300

    adds login reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,13 @@
+//This is the root reducer
+import {combineReducers} from "redux";
+import loginUserReducer from "./authReducer";
+
+//root reducer will add keys to the global application state, that are passed
+//down to the container components as props. The keys are the states returned
+//by the reducers
+
+const rootReducer = combineReducers({
+  user:loginUserReducer //loginReducer will return a user object with a token key
+});
+
+export default rootReducer;
