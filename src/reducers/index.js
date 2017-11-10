9cc9c534076513792cd7dc320e5218d673acee73
kasulani <kasulani@gmail.com>
2017-11-10 17:32:05 +0300

    adds register reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,14 @@
 //This is the root reducer
 import {combineReducers} from "redux";
-import loginUserReducer from "./authReducer";
+import {loginUserReducer, registerUser} from "./authReducer";
 
 //root reducer will add keys to the global application state, that are passed
 //down to the container components as props. The keys are the states returned
 //by the reducers
 
 const rootReducer = combineReducers({
-  user:loginUserReducer //loginReducer will return a user object with a token key
+  user: loginUserReducer, //loginReducer will return a user object with a token key
+  newUser: registerUser
 });
 
 export default rootReducer;
