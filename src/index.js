2847fa3f4928174034b049119c02f2816f0a3346
kasulani <kasulani@gmail.com>
2017-11-10 17:30:52 +0300

    adds toastr css files

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import {Router, browserHistory} from 'react-router';
 import routes from './routes';
 import './styles/shoplist.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/toastr/build/toastr.min.css';
 import configStore from "./store/configStore";
 import {Provider} from "react-redux";
 
