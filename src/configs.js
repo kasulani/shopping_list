4728cebb559cbbc8ffd85c2ad9a746a5c07118ae
kasulani <kasulani@gmail.com>
2017-11-10 17:30:23 +0300

    adds toastr config

--- src/configs.js
+++ src/configs.js
@@ -4,3 +4,18 @@ export const AXIOS_INSTANCE = axios.create({
   baseURL: 'http://127.0.0.1:5000',
   headers: {'Content-Type': 'application/json'}
 });
+
+export const TOASTR_CONFIG = {
+      "positionClass": "toast-top-center",
+      "closeButton": true,
+      "newestOnTop": false,
+      "preventDuplicates": true,
+      "showDuration": "300",
+      "hideDuration": "1000",
+      "timeOut": "5000",
+      "extendedTimeOut": "1000",
+      "showEasing": "swing",
+      "hideEasing": "linear",
+      "showMethod": "fadeIn",
+      "hideMethod": "fadeOut"
+  };
