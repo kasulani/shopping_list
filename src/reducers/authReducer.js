54081f819a1dd01c54202ce154522a193193cfd9
kasulani <kasulani@gmail.com>
2017-11-27 09:01:36 +0300

    adds reducer for logout, resetpassword

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -2,24 +2,52 @@
  *
  * Actions are handled in a reducer. A reducer is a function that accepts an
  * action and a state and returns a new state */
-import { LOGIN, REGISTER } from "../actions/types";
+import { LOGIN, REGISTER, RESET_PASSWORD, LOG_OUT } from "../actions/types";
 
-export function loginUserReducer(state = {}, action){
+export function authUserReducer(state = {}, action){
   switch (action.type) {
     case LOGIN:
       //return [...state, action.payload.data];
       return action.payload.data;
+    case LOG_OUT:
+      return action.payload;
+    //   return [...state, action.payload.data];
     default:
       return state;
   }
 }
 
-export function registerUser(state = {}, action){
+export function registerUserReducer(state = {}, action){
   switch (action.type) {
     case REGISTER:
-      //return [...state, action.payload.data];
       return action.payload.data;
+    case LOG_OUT:
+      return {};
     default:
       return state;
   }
 }
+
+export function resetPasswordReducer(state = {}, action){
+  switch (action.type) {
+    case RESET_PASSWORD:
+      return action.payload.data;
+    case LOG_OUT:
+      return {};
+    default:
+      return state;
+  }
+}
+
+// export function logOutUserReducer(state = {}, action){
+//   // debugger;
+//   // console.log("log out");
+//   switch (action.type) {
+//
+//     case LOG_OUT:
+//       // return {user: {token: ""}};
+//       return action.payload;
+//     default:
+//       return state;
+//   }
+// }
