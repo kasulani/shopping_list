b6a3181880f6c2a225a30420be8c67ef0824b1d1
kasulani <kasulani@gmail.com>
2017-11-27 08:57:43 +0300

    adds states for the CRUD operations

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,36 @@
 //This is the root reducer
 import {combineReducers} from "redux";
-import {loginUserReducer, registerUser} from "./authReducer";
+import {
+  authUserReducer,
+  registerUserReducer, resetPasswordReducer} from "./authReducer";
+import {
+  getShoppingListsReducer,
+  addNewShoppingListReducer,
+  getShoppingListReducer,
+  editShoppingListReducer, addNewItemReducer, getShoppingListItemsReducer,
+  editShoppingListItemReducer, deleteShoppingListReducer, getUserReducer,
+  deleteShoppingListItemReducer, editUserReducer
+} from "./shoppingListsReducer";
 
 //root reducer will add keys to the global application state, that are passed
 //down to the container components as props. The keys are the states returned
 //by the reducers
 
 const rootReducer = combineReducers({
-  user: loginUserReducer, //loginReducer will return a user object with a token key
-  newUser: registerUser
+  user: authUserReducer, //loginReducer will return a user object with a token key
+  newUser: registerUserReducer,
+  shoppingLists: getShoppingListsReducer, // this a list of shopping lists
+  aShoppingList: getShoppingListReducer, // this is a single shopping list
+  newShoppingList: addNewShoppingListReducer, // this will have a response object after the API call
+  editedShoppingList: editShoppingListReducer,
+  newShoppingListItem: addNewItemReducer,
+  shoppingListItems: getShoppingListItemsReducer,
+  editedListItem: editShoppingListItemReducer,
+  deletedShoppingList: deleteShoppingListReducer,
+  deletedShoppingListItem: deleteShoppingListItemReducer,
+  userDetails: getUserReducer,
+  editedUser: editUserReducer,
+  resetPass: resetPasswordReducer
 });
 
 export default rootReducer;
