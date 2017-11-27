5ed64f468e7597e69a80e6b60ac53c0935e29b41
kasulani <kasulani@gmail.com>
2017-11-27 15:53:15 +0300

    refactors component to add ids to the tags

--- src/components/common/SimplePager.js
+++ src/components/common/SimplePager.js
@@ -4,20 +4,22 @@ import PropTypes from 'prop-types';
 const SimplePager = (props) => {
   if (props.show === 'true') {
     return(
-      <nav aria-label="...">
-        <button
+      <nav id="outterPagerNavWrapper" aria-label="...">
+        <button id="previousBtn"
           onClick={props.onPrevButtonClick}
           className="btn btn-xs btn-default pull-left"
           disabled={(props.prev_page_state === 'none')?"disabled":""}>
-          <span className="glyphicon glyphicon-triangle-left" aria-hidden="true">
+          <span id="previousBtnSpan"
+            className="glyphicon glyphicon-triangle-left" aria-hidden="true">
           </span> PREVIOUS PAGE
         </button>
-        <button
+        <button id="nextBtn"
           onClick={props.onNextButtonClick}
           className="btn btn-xs btn-default pull-right"
           disabled={(props.next_page_state === 'none')?"disabled":""}>
           NEXT PAGE
-          <span className="glyphicon glyphicon-triangle-right" aria-hidden="true">
+          <span id="nextBtnSpan"
+            className="glyphicon glyphicon-triangle-right" aria-hidden="true">
           </span>
         </button>
       </nav>
