c843a6a5ec9927d29ab8f774405116b6bd1de101
kasulani <kasulani@gmail.com>
2017-11-27 09:14:46 +0300

    adds component to list shopping lists

---
+++ src/components/common/ListItemForm.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import TextInput from './TextInput';
+import TextArea from './TextArea';
+
+const ListItemForm = (props) => {
+  return(
+    <form id={props.formId} onSubmit={props.onFormSubmit}>
+      <TextInput
+        label="Item name"
+        name="itemName"
+        type="text"
+        placeholder="Enter a unique name for your item"
+        error={props.ValidationErrorsOfNameField}
+        value={props.ValueOfNameField}
+        onChange={props.onChangeText}
+      />
+      <TextArea
+        label="Description"
+        name="itemDescription"
+        placeholder="Enter a short description about your item"
+        rows="3"
+        error={props.ValidationErrorsOfDescriptionField}
+        value={props.ValueOfDescriptionField}
+        onChange={props.onChangeText}
+      />
+    </form>
+  );
+};
+
+ListItemForm.propTypes = {
+  formId: PropTypes.string.isRequired,
+  ValidationErrorsOfNameField: PropTypes.string,
+  ValidationErrorsOfDescriptionField: PropTypes.string,
+  ValueOfNameField: PropTypes.string.isRequired,
+  ValueOfDescriptionField: PropTypes.string.isRequired,
+  onChangeText: PropTypes.func.isRequired,
+  onFormSubmit: PropTypes.func.isRequired
+};
+
+export default ListItemForm;
