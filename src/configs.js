a9d4f0e9cae5728133b339ab88fafa70f11f3956
kasulani <kasulani@gmail.com>
2017-11-27 09:04:23 +0300

    refactors toastr config

--- src/configs.js
+++ src/configs.js
@@ -1,21 +1,42 @@
 import axios from "axios";
+import store from "./store/configStore";
+import { browserHistory } from 'react-router';
+
+export function errorHandler(statusCode) {
+  switch (statusCode) {
+    case 404:
+      // handle 404 error
+      browserHistory.push('/error/404');
+      break;
+    default:
+  }
+}
 
 export const AXIOS_INSTANCE = axios.create({
   baseURL: 'http://127.0.0.1:5000',
   headers: {'Content-Type': 'application/json'}
 });
 
+AXIOS_INSTANCE.interceptors.request.use((config) => {
+  const token = store.getState().user.token;
+  if (token) {
+    config.headers.authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+
 export const TOASTR_CONFIG = {
-      "positionClass": "toast-top-center",
-      "closeButton": true,
-      "newestOnTop": false,
-      "preventDuplicates": true,
-      "showDuration": "300",
-      "hideDuration": "1000",
-      "timeOut": "5000",
-      "extendedTimeOut": "1000",
-      "showEasing": "swing",
-      "hideEasing": "linear",
-      "showMethod": "fadeIn",
-      "hideMethod": "fadeOut"
-  };
+  "positionClass": "toast-top-center",
+  "closeButton": true,
+  "newestOnTop": false,
+  "preventDuplicates": true,
+  "showDuration": "300",
+  "hideDuration": "1000",
+  "timeOut": "5000",
+  "extendedTimeOut": "1000",
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "fadeIn",
+  "hideMethod": "fadeOut"
+};
