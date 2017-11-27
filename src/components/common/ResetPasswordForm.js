7e31a3b4254e95a6aade94075544f51e31b2baa5
kasulani <kasulani@gmail.com>
2017-11-27 09:18:24 +0300

    adds component for reset password

---
+++ src/components/common/ResetPasswordForm.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import TextInput from './TextInput';
+
+const ResetPasswordForm = (props) => {
+  return(
+    <form id={props.formId} onSubmit={props.onFormSubmit}>
+      <TextInput
+        label="Old password"
+        name="oldPassword"
+        type="password"
+        placeholder="Enter your old password here"
+        error={props.ValidationErrorsOfOldPasswordField}
+        value={props.ValueOfOldPasswordField}
+        onChange={props.onChangeText}
+      />
+      <TextInput
+        label="New password"
+        name="newPassword"
+        type="password"
+        placeholder="Enter your new password here"
+        error={props.ValidationErrorsOfNewPasswordField}
+        value={props.ValueOfNewPasswordField}
+        onChange={props.onChangeText}
+      />
+      <TextInput
+        label="Confirm password"
+        name="confirmPassword"
+        type="password"
+        placeholder="Enter your new password here again"
+        error={props.ValidationErrorsOfConfirmPasswordField}
+        value={props.ValueOfConfirmPasswordField}
+        onChange={props.onChangeText}
+      />
+    </form>
+  );
+};
+
+ResetPasswordForm.propTypes = {
+  formId: PropTypes.string.isRequired,
+  ValidationErrorsOfOldPasswordField: PropTypes.string,
+  ValidationErrorsOfNewPasswordField: PropTypes.string,
+  ValidationErrorsOfConfirmPasswordField: PropTypes.string,
+  ValueOfOldPasswordField: PropTypes.string.isRequired,
+  ValueOfNewPasswordField: PropTypes.string.isRequired,
+  ValueOfConfirmPasswordField: PropTypes.string.isRequired,
+  onChangeText: PropTypes.func.isRequired,
+  onFormSubmit: PropTypes.func.isRequired
+};
+
+export default ResetPasswordForm;
