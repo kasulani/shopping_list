f7604a160bfea36c1b4098a70ec72489de2b2f05
kasulani <kasulani@gmail.com>
2017-11-27 23:44:30 +0300

    adds test to cover line 28 of simple pager component

--- src/tests/common/SimplePager.test.js
+++ src/tests/common/SimplePager.test.js
@@ -6,19 +6,20 @@ import SimplePager from "../../components/common/SimplePager";
 let onPrevButtonClickCalled = false;
 let onNextButtonClickCalled = false;
 
-function setup(){
+function setup(prev_state, next_state, show){
   const props ={
     onPrevButtonClick: () =>{ onPrevButtonClickCalled = true; },
-    prev_page_state:"/example.com/?page=1&limit=2",
+    prev_page_state:prev_state,
     onNextButtonClick: () =>{ onNextButtonClickCalled = true; },
-    next_page_state:"/example.com/?page=2&limit=2",
-    show: "true"
+    next_page_state:next_state,
+    show: show
   };
   return shallow(<SimplePager {...props}/>);
 }
 
 describe('Run tests on SimplePager component', () => {
-  const simplePager = setup();
+  const simplePager =
+  setup("/example.com/?page=1&limit=2", "/example.com/?page=2&limit=2", "true");
   // test previous button
   it('has the correct button bootstrap class for the previous button', () => {
     expect(simplePager.find('#previousBtn').props().className).
@@ -53,8 +54,13 @@ describe('Run tests on SimplePager component', () => {
   it('shows the correct button text', () => {
     expect(simplePager.find('#nextBtn').text()).toEqual('NEXT PAGE');
   });
-  it('can call the next button on click handler', () => {
-    simplePager.find('#nextBtn').simulate('click');
-    expect(onNextButtonClickCalled).toEqual(true);
+  it('can disable pager buttons if no page', () => {
+    const pager = setup('none', 'none', 'true');
+    expect(pager.find('#previousBtn').props().disabled).toEqual('disabled');
+    expect(pager.find('#nextBtn').props().disabled).toEqual('disabled');
+  });
+  it('can not render a pager if the show property is false', () => {
+    const pager = setup('none', 'none', 'false');
+    expect(pager.find('nav').length).toBe(1);
   });
 });
