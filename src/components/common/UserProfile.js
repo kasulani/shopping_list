97dc6adf09018ed5dff75a203c92aa6e3a3a4219
kasulani <kasulani@gmail.com>
2017-11-27 23:25:13 +0300

    refactored component to add ids for easy testing

--- src/components/common/UserProfile.js
+++ src/components/common/UserProfile.js
@@ -10,13 +10,13 @@ const UserProfile = (props) => {
         <div className="box box-primary">
           <div className="box-body box-profile">
             {/*<img id="profile_picture" className="profile-user-img img-responsive img-circle" src="#" onerror="this.src='{{url_for('static', filename='img/profile.png')}}';" alt="profile picture">*/}
-            <h3 className="profile-username text-center"><small>{props.username}</small></h3>
+            <h3 id="profileName" className="profile-username text-center"><small>{props.username}</small></h3>
             <ul className="list-group list-group-unbordered">
               <li className="list-group-item">
-                <b>Shopping Lists</b> <a className="pull-right">{props.lists}</a>
+                <b>Shopping Lists</b> <a id="listCount" className="pull-right">{props.lists}</a>
               </li>
               <li className="list-group-item">
-                <b>Total Items</b> <a className="pull-right">{props.items}</a>
+                <b>Total Items</b> <a id="itemCount" className="pull-right">{props.items}</a>
               </li>
               {/*<li className="list-group-item">
               <b>Friends</b> <a className="pull-right">0</a>
@@ -30,7 +30,7 @@ const UserProfile = (props) => {
           <div className="box-header with-border">
             <h3 className="box-title"><span className="glyphicon glyphicon-user"></span> About Me</h3>
           </div>{/*box-header*/}
-          <div className="box-body">
+          <div id="profileDescription" className="box-body">
             <p>{(!props.description) ? "Add something about yourself, please head over to your profile and complete it.": props.description}</p>
           </div>{/*box-body*/}
         </div>{/*box*/}
