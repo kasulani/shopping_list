dd1fef88308624d89c9d337a4f4e90ce18316df4
kasulani <kasulani@gmail.com>
2017-11-27 09:02:50 +0300

    refactors import of store

--- src/index.js
+++ src/index.js
@@ -4,12 +4,10 @@ import {render} from 'react-dom';
 import {Router, browserHistory} from 'react-router';
 import routes from './routes';
 import './styles/shoplist.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+//import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/toastr/build/toastr.min.css';
-import configStore from "./store/configStore";
+import store from "./store/configStore";
 import {Provider} from "react-redux";
-
-const store = configStore();
 
 render(
   <Provider store={store}>
