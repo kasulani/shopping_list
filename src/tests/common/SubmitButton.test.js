45639ef6f9ef83e2065d75d0a9aa9e095a7dd0bd
kasulani <kasulani@gmail.com>
2017-11-27 23:50:23 +0300

    adds test to cover line 7 of the submitButton component

--- src/tests/common/SubmitButton.test.js
+++ src/tests/common/SubmitButton.test.js
@@ -3,26 +3,31 @@ import React from "react";
 import {mount, shallow} from "enzyme";
 import SubmitButton from "../../components/common/SubmitButton";
 
-function setup(){
+function setup(btnClass){
   const props ={
-    formId:"testForm", buttonText:"Save"
+    formId:"testForm", buttonText:"Save", buttonClass: btnClass
   };
   return shallow(<SubmitButton {...props}/>);
 }
 
 describe('Run tests on SubmitButton component', () => {
-  const textField = setup();
+  const submitBtn = setup("");
   it('has the correct button bootstrap class', () => {
-    expect(textField.find('button').props().className).
+    expect(submitBtn.find('button').props().className).
     toEqual('btn btn-sm btn-primary pull-right');
   });
   it('has the correct form id', () => {
-    expect(textField.find('button').props().form).toEqual('testForm');
+    expect(submitBtn.find('button').props().form).toEqual('testForm');
   });
   it('has the correct button type set as submit', () => {
-    expect(textField.find('button').props().type).toEqual('submit');
+    expect(submitBtn.find('button').props().type).toEqual('submit');
   });
   it('shows the correct button text', () => {
-    expect(textField.find('button').text()).toEqual('Save');
+    expect(submitBtn.find('button').text()).toEqual('Save');
+  });
+  it('can change the button bootstrap class', () => {
+    const btn = setup("btn btn-sm btn-primary pull-left");
+    expect(btn.find('button').props().className).
+    toEqual('btn btn-sm btn-primary pull-left');
   });
 });
