795383d61a6f7593d32d3cfa20ded84c75ad27f9
kasulani <kasulani@gmail.com>
2017-12-17 14:57:26 +0300

    refactors the api endpoint

--- src/configs.js
+++ src/configs.js
@@ -13,7 +13,7 @@ export function errorHandler(statusCode) {
 }
 
 export const AXIOS_INSTANCE = axios.create({
-  baseURL: 'http://127.0.0.1:5000',
+  baseURL: 'http://127.0.0.1:5000/v1',
   headers: {'Content-Type': 'application/json'}
 });
 
